156a6b4cf9aa768ffd3ce7e84207392b817496b2
Truong Minh Duc <ypvt123@gmail.com>
2023-08-02 13:21:48 +0700

    feat: Add CRUD endpoints for posts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"Posts-Demo/controllers"
 	"Posts-Demo/inits"
 
 	"github.com/gin-gonic/gin"
@@ -20,6 +21,16 @@ func main() {
 			"message": "Hello World!",
 		})
 	})
+
+	r.POST("/post", controllers.CreatePost)
+
+	r.GET("/posts", controllers.GetPosts)
+
+	r.GET("/post/:id", controllers.GetPost)
+
+	r.PUT("/post/:id", controllers.UpdatePost)
+
+	r.DELETE("/post/:id", controllers.DeletePost)
 
 	r.Run()
 }
